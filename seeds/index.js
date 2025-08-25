aef9d495afc502f2b259056ef1d01ede627b4a1b
Ayush Kumar Singh <aayushsingh5526@gmail.com>
2025-08-25 16:14:50 +0530

    new commit

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const DB_URL = process.env.DB_URL;
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(DB_URL, {
 
 });
 
